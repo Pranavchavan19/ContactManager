119f04f867105a8305c063ea14568fb3f61dfcb1
Pranavbc <pranav1952000@gmail.com>
2025-07-10 18:06:09 +0530

    git

--- src/main/java/com/scm/config/AppConfig.java
+++ src/main/java/com/scm/config/AppConfig.java
@@ -1,18 +1,15 @@
 package com.scm.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.web.session.DisableEncodeUrlFilter;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
 @Configuration
 public class AppConfig {
-
-
 
     @Value("${CLOUDINARY_NAME}")
     private String cloudName;
@@ -23,19 +20,21 @@ public class AppConfig {
     @Value("${CLOUDINARY_API_SECRET}")
     private String apiSecret;
 
+    @PostConstruct
+    public void debugEnv() {
+        System.out.println("DEBUG ENV:");
+        System.out.println("CLOUDINARY_NAME = " + cloudName);
+        System.out.println("CLOUDINARY_API_KEY = " + apiKey);
+        System.out.println("CLOUDINARY_API_SECRET = " + apiSecret);
+    }
 
     @Bean
     public Cloudinary cloudinary() {
-
         return new Cloudinary(
-
                 ObjectUtils.asMap(
                         "cloud_name", cloudName,
                         "api_key", apiKey,
                         "api_secret", apiSecret)
-
         );
-
     }
-
 }
