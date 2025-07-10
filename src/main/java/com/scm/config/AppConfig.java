e7a0cdb5d18d4e94b2d1b875aecf812c3d062210
Pranavbc <pranav1952000@gmail.com>
2025-07-10 23:47:36 +0530

    git12

--- src/main/java/com/scm/config/AppConfig.java
+++ src/main/java/com/scm/config/AppConfig.java
@@ -1,9 +1,10 @@
 package com.scm.config;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.web.session.DisableEncodeUrlFilter;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
@@ -11,30 +12,27 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class AppConfig {
 
-    @Value("${CLOUDINARY_NAME}")
+    @Value("${cloudinary.cloud.name}")
     private String cloudName;
 
-    @Value("${CLOUDINARY_API_KEY}")
+    @Value("${cloudinary.api.key}")
     private String apiKey;
 
-    @Value("${CLOUDINARY_API_SECRET}")
+    @Value("${cloudinary.api.secret}")
     private String apiSecret;
-
-    @PostConstruct
-    public void debugEnv() {
-        System.out.println("DEBUG ENV:");
-        System.out.println("CLOUDINARY_NAME = " + cloudName);
-        System.out.println("CLOUDINARY_API_KEY = " + apiKey);
-        System.out.println("CLOUDINARY_API_SECRET = " + apiSecret);
-    }
 
     @Bean
     public Cloudinary cloudinary() {
+
         return new Cloudinary(
+
                 ObjectUtils.asMap(
                         "cloud_name", cloudName,
                         "api_key", apiKey,
                         "api_secret", apiSecret)
+
         );
+
     }
+
 }
