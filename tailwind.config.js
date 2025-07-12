dfb9269fb6210be0956d45805aacb963d5424929
Pranavbc <pranav1952000@gmail.com>
2025-07-12 18:11:35 +0530

    cache issu on render

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
+///** @type {import('tailwindcss').Config} */
+//export default {
+//  content: ["./src/main/resources/**/*.{html,js}"],
+//  theme: {
+//    extend: {},
+//  },
+//  plugins: [],
+//  darkMode: "selector",
+//};
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/main/resources/**/*.{html,js}"],
+  content: ["./src/main/resources/templates/**/*.html"],
   theme: {
     extend: {},
   },
